ed0e3281faaf568ff2454433960a62acd5bde82b
james61124 <james61124@gmail.com>
2023-12-31 04:28:27 +0800

    open log

--- home-python/multimedia/multimedia/urls.py
+++ home-python/multimedia/multimedia/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from journal import views
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls.static import static #
 
 urlpatterns = [
     path('admin', admin.site.urls),
